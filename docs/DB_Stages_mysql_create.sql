c996b04b2d4d964cd5d1fe97ef01dd474e4d6dfe
brjousse <brjousse@turing.local.isima.fr>
2019-12-18 12:18:07 +0100

    Ajout bd

--- docs/DB_Stages_mysql_create.sql
+++ docs/DB_Stages_mysql_create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `Etudiant` (
 	`id_etud` int NOT NULL AUTO_INCREMENT,
 	`id_util` int NOT NULL,
-	`id_form` int NOT NULL,
+	`annee` int NOT NULL,
+	`departement` int NOT NULL,
 	`option` enum NOT NULL,
 	`promo` int NOT NULL,
 	PRIMARY KEY (`id_etud`)
@@ -9,14 +10,11 @@ CREATE TABLE `Etudiant` (
 
 CREATE TABLE `Stage` (
 	`id_stage` int NOT NULL AUTO_INCREMENT,
-	`id_resp` int NOT NULL,
-	`id_etud` int NOT NULL,
 	`id_form` int NOT NULL,
 	`id_ent` int NOT NULL,
 	`id_adr` int NOT NULL,
 	`sujet` varchar(255) NOT NULL,
 	`intitule` varchar(255),
-	`option` enum NOT NULL,
 	`promo` int NOT NULL,
 	`annee` int NOT NULL,
 	`duree` int NOT NULL,
@@ -26,6 +24,13 @@ CREATE TABLE `Stage` (
 	`theme` varchar(255),
 	`contratPro` bool NOT NULL,
 	`commentaire` varchar(1024),
+	`nom_etud` varchar(255) NOT NULL,
+	`prenom_etud` varchar(255) NOT NULL,
+	`gratification_val` int,
+	`departement` varchar(255) NOT NULL,
+	`annee_enum` enum NOT NULL,
+	`nom_tuteur` varchar(255),
+	`prenom_tuteur` varchar(255),
 	PRIMARY KEY (`id_stage`)
 );
 
@@ -49,6 +54,8 @@ CREATE TABLE `Adresse` (
 	`ville` varchar(255) NOT NULL,
 	`latitude` int NOT NULL,
 	`longitude` int NOT NULL,
+	`continent` varchar(255) NOT NULL,
+	`estPrivee` bool NOT NULL,
 	PRIMARY KEY (`id_adr`)
 );
 
@@ -62,29 +69,24 @@ CREATE TABLE `Responsable` (
 	PRIMARY KEY (`id_resp`)
 );
 
-CREATE TABLE `Formation` (
-	`id_form` int NOT NULL AUTO_INCREMENT,
-	`annee` enum NOT NULL,
-	`filiere` enum NOT NULL,
-	PRIMARY KEY (`id_form`)
-);
-
 CREATE TABLE `Pays` (
 	`id_pays` int NOT NULL AUTO_INCREMENT,
 	`pays` varchar(255) NOT NULL UNIQUE,
 	PRIMARY KEY (`id_pays`)
 );
 
-CREATE TABLE `Responsable_Formation` (
-	`id_form` bigint NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Formation` (
+	`annee` int NOT NULL,
+	`departement` int NOT NULL,
 	`id_resp` int NOT NULL,
-	PRIMARY KEY (`id_form`,`id_resp`)
+	PRIMARY KEY (`annee`,`departement`,`id_resp`)
 );
 
-CREATE TABLE `Stage_MotCle` (
+CREATE TABLE `MotCle` (
 	`id_stage` int NOT NULL AUTO_INCREMENT,
 	`motCle` varchar(255) NOT NULL UNIQUE,
-	PRIMARY KEY (`id_stage`)
+	`id_pere` int,
+	PRIMARY KEY (`id_stage`,`motCle`)
 );
 
 CREATE TABLE `Utilisateur` (
@@ -99,23 +101,23 @@ CREATE TABLE `Utilisateur` (
 
 CREATE TABLE `Utilisateur_Droit` (
 	`id_util` int NOT NULL,
+	`id_droit` int NOT NULL,
+	PRIMARY KEY (`id_util`,`id_droit`)
+);
+
+CREATE TABLE `Droit` (
+	`id_droit` int NOT NULL AUTO_INCREMENT,
 	`droit` enum NOT NULL,
-	PRIMARY KEY (`id_util`)
+	PRIMARY KEY (`id_droit`)
 );
 
 ALTER TABLE `Etudiant` ADD CONSTRAINT `Etudiant_fk0` FOREIGN KEY (`id_util`) REFERENCES `Utilisateur`(`id_util`);
 
-ALTER TABLE `Etudiant` ADD CONSTRAINT `Etudiant_fk1` FOREIGN KEY (`id_form`) REFERENCES `Formation`(`id_form`);
+ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk0` FOREIGN KEY (`id_form`) REFERENCES `Formation`(`id_form`);
 
-ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk0` FOREIGN KEY (`id_resp`) REFERENCES `Responsable`(`id_resp`);
+ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk1` FOREIGN KEY (`id_ent`) REFERENCES `Entreprise`(`id_ent`);
 
-ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk1` FOREIGN KEY (`id_etud`) REFERENCES `Etudiant`(`id_etud`);
-
-ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk2` FOREIGN KEY (`id_form`) REFERENCES `Formation`(`id_form`);
-
-ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk3` FOREIGN KEY (`id_ent`) REFERENCES `Entreprise`(`id_ent`);
-
-ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk4` FOREIGN KEY (`id_adr`) REFERENCES `Adresse`(`id_adr`);
+ALTER TABLE `Stage` ADD CONSTRAINT `Stage_fk2` FOREIGN KEY (`id_adr`) REFERENCES `Adresse`(`id_adr`);
 
 ALTER TABLE `Adresse` ADD CONSTRAINT `Adresse_fk0` FOREIGN KEY (`id_ent`) REFERENCES `Entreprise`(`id_ent`);
 
@@ -123,11 +125,13 @@ ALTER TABLE `Adresse` ADD CONSTRAINT `Adresse_fk1` FOREIGN KEY (`id_pays`) REFER
 
 ALTER TABLE `Responsable` ADD CONSTRAINT `Responsable_fk0` FOREIGN KEY (`id_util`) REFERENCES `Utilisateur`(`id_util`);
 
-ALTER TABLE `Responsable_Formation` ADD CONSTRAINT `Responsable_Formation_fk0` FOREIGN KEY (`id_form`) REFERENCES `Formation`(`id_form`);
+ALTER TABLE `Formation` ADD CONSTRAINT `Formation_fk0` FOREIGN KEY (`id_resp`) REFERENCES `Responsable`(`id_resp`);
 
-ALTER TABLE `Responsable_Formation` ADD CONSTRAINT `Responsable_Formation_fk1` FOREIGN KEY (`id_resp`) REFERENCES `Responsable`(`id_resp`);
+ALTER TABLE `MotCle` ADD CONSTRAINT `MotCle_fk0` FOREIGN KEY (`id_stage`) REFERENCES `Stage`(`id_stage`);
 
-ALTER TABLE `Stage_MotCle` ADD CONSTRAINT `Stage_MotCle_fk0` FOREIGN KEY (`id_stage`) REFERENCES `Stage`(`id_stage`);
+ALTER TABLE `MotCle` ADD CONSTRAINT `MotCle_fk1` FOREIGN KEY (`id_pere`) REFERENCES `MotCle`(`id_stage`);
 
 ALTER TABLE `Utilisateur_Droit` ADD CONSTRAINT `Utilisateur_Droit_fk0` FOREIGN KEY (`id_util`) REFERENCES `Utilisateur`(`id_util`);
+
+ALTER TABLE `Utilisateur_Droit` ADD CONSTRAINT `Utilisateur_Droit_fk1` FOREIGN KEY (`id_droit`) REFERENCES `Droit`(`id_droit`);
 
